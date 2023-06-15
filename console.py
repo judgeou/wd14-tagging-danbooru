eb649edc5441dd56c25bd12a78929a680442c313
ouzian <judgeou@vip.qq.com>
2023-06-15 10:55:55 +0800

    add replace

--- console.py
+++ console.py
@@ -10,6 +10,14 @@ def excludeTags (str):
     f.close()
     return str
 
+def replaceTags (str):
+    f = open('replace.txt', 'r')
+    for line in f.readlines():
+        pair = line.split()
+        str = str.replace(pair[0], pair[1])
+    f.close()
+    return str
+
 while True:
     clipboard_image = ImageGrab.grabclipboard()
     if clipboard_image is None:
@@ -17,6 +25,7 @@ while True:
     else:
         result = app.image_to_wd14_tags(clipboard_image, 'wd14-convnext', 0.35, False, True, False, True)
         result = excludeTags(result)
+        result = replaceTags(result)
         print(result)
         clipboard.copy(result)
     input('wait your image')
